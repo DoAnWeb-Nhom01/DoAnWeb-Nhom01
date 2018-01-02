b5833f9ea18e7318900ddeb358559ea81ebc636f
Đỗ Phạm Ngọc Thiện <globex147@gmail.com>
2018-01-02 22:24:10 +0700

    connect db

---
+++ lib/db.php
@@ -0,0 +1,46 @@
+<?php
+
+define("HOST", "127.0.0.1");
+define("DB", "qlbh");
+define("UID", "root");
+define("PWD", "");
+
+function load($sql) {
+	$cn = new mysqli(HOST, UID, PWD, DB);
+	if ($cn->connect_errno) {
+	    die("FAILED");
+	}
+
+	// echo $cn->host_info . "\n";
+	$cn->query("set names 'utf8'");
+	$rs = $cn->query($sql);
+	return $rs;
+}
+
+function write($sql) {
+	$cn = new mysqli(HOST, UID, PWD, DB);
+	if ($cn->connect_errno) {
+	    die("FAILED");
+	}
+
+	$cn->query("set names 'utf8'");
+	$cn->query($sql);
+	
+}
+
+function insertRow($sql) {
+	$cn = new mysqli(HOST, UID, PWD, DB);
+	if ($cn->connect_errno) {
+	    die("FAILED");
+	}
+
+	$cn->query("set names 'utf8'");
+
+	$cn->query($sql);
+
+	$insertId = $cn->insert_id;
+
+	mysqli_close($cn);
+
+	return $insertId;
+}
